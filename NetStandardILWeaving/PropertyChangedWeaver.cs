0207c0568a5345d2c9d027473982efdaf6180e90
Sebastian Andraos <s.andraos@hal-robotics.com>
2017-03-19 13:51:54 +0000

    Initial Code

---
+++ NetStandardILWeaving/PropertyChangedWeaver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Blur;
+
+[assembly: Blur(CleanUp = true)]
+
+namespace NetStandardILWeaving
+{
+    class PropertyChangedWeaver : BlurVisitor
+    {
+        public override int Priority => 900;
+    }
+}
